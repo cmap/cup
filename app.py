68b498b478abf3452c89672b09efb589ccd7a75f
jdavis <jdavis@broadinstitute.org>
2022-11-30 16:26:22 -0500

    Added check for build existance prior to running

--- app.py
+++ app.py
@@ -46,14 +46,6 @@ builds_list = []
 for i in builds:
     # insert filter IF here
     builds_list.append(list(i.values())[0])
-print(builds_list)
-
-# build_list = fs.ls('s3://macchiato.clue.io/builds')
-# builds = []
-# for i in build_list:
-#    res = i.split('/')[2]
-#    builds.append(res)
-
 
 # USER INPUTS
 
@@ -81,117 +73,120 @@ def get_qc_table(files):
 if run and build:
 
     build_path = "s3://macchiato.clue.io/builds/" + build + "/build/"
-    file_list = fs.ls(build_path)
-    qc_file = 's3://' + get_qc_table(file_list)
-    print('QC file found: ' + qc_file)
-    mfi_file = 's3://' + get_lvl3(file_list)
-    print('MFI file found: ' + mfi_file)
+    if fs.exists(build_path):
+        file_list = fs.ls(build_path)
+        qc_file = 's3://' + get_qc_table(file_list)
+        print('QC file found: ' + qc_file)
+        mfi_file = 's3://' + get_lvl3(file_list)
+        print('MFI file found: ' + mfi_file)
 
-    with st.spinner('Generating report...'):
-        # read data
-        mfi_cols = ['prism_replicate', 'pool_id', 'ccle_name', 'culture', 'pert_type', 'pert_well', 'replicate',
-                    'logMFI_norm', 'logMFI', 'pert_plate']
-        qc_cols = ['prism_replicate', 'ccle_name', 'pool_id', 'culture', 'pert_plate', 'ctl_vehicle_md',
-                   'trt_poscon_md', 'ctl_vehicle_mad', 'trt_poscon_mad', 'ssmd', 'nnmd', 'error_rate', 'dr', 'pass']
+        with st.spinner('Generating report...'):
+            # read data
+            mfi_cols = ['prism_replicate', 'pool_id', 'ccle_name', 'culture', 'pert_type', 'pert_well', 'replicate',
+                        'logMFI_norm', 'logMFI', 'pert_plate']
+            qc_cols = ['prism_replicate', 'ccle_name', 'pool_id', 'culture', 'pert_plate', 'ctl_vehicle_md',
+                       'trt_poscon_md', 'ctl_vehicle_mad', 'trt_poscon_mad', 'ssmd', 'nnmd', 'error_rate', 'dr', 'pass']
 
-        qc = pd.read_csv(qc_file, usecols=qc_cols)
-        mfi = pd.read_csv(mfi_file, usecols=mfi_cols)
+            qc = pd.read_csv(qc_file, usecols=qc_cols)
+            mfi = pd.read_csv(mfi_file, usecols=mfi_cols)
 
-        # transform mfi dataframe
-        mfi_out = mfi.pipe(df_transform.add_bc_type)
-        # transform qc dataframe
+            # transform mfi dataframe
+            mfi_out = mfi.pipe(df_transform.add_bc_type)
+            # transform qc dataframe
 
-        qc_out = qc.pipe(df_transform.add_pass_rates)
+            qc_out = qc.pipe(df_transform.add_pass_rates)
 
-        # pivoted level for poscon/negcon comparison
-        control_df = mfi_out.pipe(df_transform.pivot_dmso_bort)
-        control_df = control_df.merge(qc_out,
-                                      on=['prism_replicate',
-                                          'ccle_name',
-                                          'pert_plate'],
-                                      how='left')
+            # pivoted level for poscon/negcon comparison
+            control_df = mfi_out.pipe(df_transform.pivot_dmso_bort)
+            control_df = control_df.merge(qc_out,
+                                          on=['prism_replicate',
+                                              'ccle_name',
+                                              'pert_plate'],
+                                          how='left')
 
-        # OUTPUT
+            # OUTPUT
 
-        st.title('QC report')
-        st.header(date.today())
+            st.title('QC report')
+            st.header(date.today())
 
-        st.header('Pass rates')
-        by_plate, by_pool = st.tabs(['By plate', 'By pool'])
-        with by_plate:
-            plotting_functions.plot_pass_rates_by_plate(qc_out)
-        with by_pool:
-            plotting_functions.plot_pass_rates_by_pool(qc_out)
+            st.header('Pass rates')
+            by_plate, by_pool = st.tabs(['By plate', 'By pool'])
+            with by_plate:
+                plotting_functions.plot_pass_rates_by_plate(qc_out)
+            with by_pool:
+                plotting_functions.plot_pass_rates_by_pool(qc_out)
 
-        st.header('QC Metrics')
-        lum_pert, ssmd = st.tabs(['Dynamic range', 'SSMD'])
-        with lum_pert:
-            plotting_functions.plot_dynamic_range(qc_out)
-        with ssmd:
-            plotting_functions.plot_ssmd(qc_out)
+            st.header('QC Metrics')
+            lum_pert, ssmd = st.tabs(['Dynamic range', 'SSMD'])
+            with lum_pert:
+                plotting_functions.plot_dynamic_range(qc_out)
+            with ssmd:
+                plotting_functions.plot_ssmd(qc_out)
 
-        st.header('Dynamic range & error rate')
-        tab_labels = qc_out.pert_plate.unique().tolist()
-        n = 0
-        for pert_plate in st.tabs(tab_labels):
-            with pert_plate:
-                plate = tab_labels[n]
-                n += 1
-                data = qc_out[qc_out.pert_plate == plate]
-                height = math.ceil(data.prism_replicate.unique().shape[0] / 3) * 400
-                plotting_functions.plot_ssmd_error_rate(data, height=height)
+            st.header('Dynamic range & error rate')
+            tab_labels = qc_out.pert_plate.unique().tolist()
+            n = 0
+            for pert_plate in st.tabs(tab_labels):
+                with pert_plate:
+                    plate = tab_labels[n]
+                    n += 1
+                    data = qc_out[qc_out.pert_plate == plate]
+                    height = math.ceil(data.prism_replicate.unique().shape[0] / 3) * 400
+                    plotting_functions.plot_ssmd_error_rate(data, height=height)
 
-        st.subheader('Pass/Fail')
-        pass_fail = df_transform.generate_pass_fail_tbl(mfi=mfi_out,
-                                                        qc=qc_out)
-        st.table(pass_fail.reset_index(drop=True).style.bar(subset=['Pass'], color='#006600', vmin=0, vmax=100).bar(
-            subset=['Fail both', 'Fail error rate', 'Fail dynamic range'], color='#d65f5f', vmin=0, vmax=100))
+            st.subheader('Pass/Fail')
+            pass_fail = df_transform.generate_pass_fail_tbl(mfi=mfi_out,
+                                                            qc=qc_out)
+            st.table(pass_fail.reset_index(drop=True).style.bar(subset=['Pass'], color='#006600', vmin=0, vmax=100).bar(
+                subset=['Fail both', 'Fail error rate', 'Fail dynamic range'], color='#d65f5f', vmin=0, vmax=100))
 
-        st.header('Banana plots (raw)')
-        tab_labels = control_df.pert_plate.unique().tolist()
-        n = 0
-        for pert_plate in st.tabs(tab_labels):
-            with pert_plate:
-                plate = tab_labels[n]
-                n += 1
-                data = control_df[control_df.pert_plate == plate]
-                height = math.ceil(data.prism_replicate.unique().shape[0] / 3) * 400
-                plotting_functions.plot_banana_plots(data,
-                                                     x='ctl_vehicle_med',
-                                                     y='trt_poscon_med',
-                                                     height=height)
+            st.header('Banana plots (raw)')
+            tab_labels = control_df.pert_plate.unique().tolist()
+            n = 0
+            for pert_plate in st.tabs(tab_labels):
+                with pert_plate:
+                    plate = tab_labels[n]
+                    n += 1
+                    data = control_df[control_df.pert_plate == plate]
+                    height = math.ceil(data.prism_replicate.unique().shape[0] / 3) * 400
+                    plotting_functions.plot_banana_plots(data,
+                                                         x='ctl_vehicle_med',
+                                                         y='trt_poscon_med',
+                                                         height=height)
 
-        st.header('Banana plots (normalized)')
-        tab_labels = control_df.pert_plate.unique().tolist()
-        n = 0
-        for pert_plate in st.tabs(tab_labels):
-            with pert_plate:
-                plate = tab_labels[n]
-                n += 1
-                data = control_df[control_df.pert_plate == plate]
-                height = math.ceil(data.prism_replicate.unique().shape[0] / 3) * 400
-                plotting_functions.plot_banana_plots(data,
-                                                     x='ctl_vehicle_med_norm',
-                                                     y='trt_poscon_med_norm',
-                                                     height=height)
+            st.header('Banana plots (normalized)')
+            tab_labels = control_df.pert_plate.unique().tolist()
+            n = 0
+            for pert_plate in st.tabs(tab_labels):
+                with pert_plate:
+                    plate = tab_labels[n]
+                    n += 1
+                    data = control_df[control_df.pert_plate == plate]
+                    height = math.ceil(data.prism_replicate.unique().shape[0] / 3) * 400
+                    plotting_functions.plot_banana_plots(data,
+                                                         x='ctl_vehicle_med_norm',
+                                                         y='trt_poscon_med_norm',
+                                                         height=height)
 
-        st.header('Liver plots')
-        tab_labels = qc_out.pert_plate.unique().tolist()
-        n = 0
-        for pert_plate in st.tabs(tab_labels):
-            with pert_plate:
-                plate = tab_labels[n]
-                n += 1
-                plotting_functions.plot_liver_plots(qc_out[qc_out.pert_plate == plate])
+            st.header('Liver plots')
+            tab_labels = qc_out.pert_plate.unique().tolist()
+            n = 0
+            for pert_plate in st.tabs(tab_labels):
+                with pert_plate:
+                    plate = tab_labels[n]
+                    n += 1
+                    plotting_functions.plot_liver_plots(qc_out[qc_out.pert_plate == plate])
 
-        st.header('Plate distributions')
-        tab_labels = mfi_out.pert_plate.unique().tolist()
-        n = 0
-        for pert_plate in st.tabs(tab_labels):
-            with pert_plate:
-                plate = tab_labels[n]
-                n += 1
-                data = mfi_out[mfi_out.pert_plate == plate]
-                height = math.ceil(data.prism_replicate.unique().shape[0] / 3) * 400
-                plotting_functions.plot_distributions_by_plate(data,
-                                                               height=height)
+            st.header('Plate distributions')
+            tab_labels = mfi_out.pert_plate.unique().tolist()
+            n = 0
+            for pert_plate in st.tabs(tab_labels):
+                with pert_plate:
+                    plate = tab_labels[n]
+                    n += 1
+                    data = mfi_out[mfi_out.pert_plate == plate]
+                    height = math.ceil(data.prism_replicate.unique().shape[0] / 3) * 400
+                    plotting_functions.plot_distributions_by_plate(data,
+                                                                   height=height)
+    else:
+        st.text('Build does not exist; check S3.')
