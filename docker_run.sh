e11eb333333ab6306c9c5b53503d1cea442495ff
jasiedu <jasiedu@broadinstitute.org>
2024-07-28 16:01:46 -0400

    Adding JWT authentication

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 docker run -p 8801:8801 \
+-e API_KEY=XXXXXXX \
+-e API_URL="https://api.clue.io/api/" \
+-e AWS_ACCESS_KEY_ID=XXXXXXX \
+-e AWS_SECRET_ACCESS_KEY=XXXXXXXX \
+-e AWS_DEFAULT_REGION=us-east-1 \
 -it prismcmap/cup-dev
